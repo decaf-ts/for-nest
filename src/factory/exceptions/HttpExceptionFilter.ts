63e536bd70b8be463790c2bf6f3e7b44b36a18e4
Pedro Costa <pcesar_costa@hotmail.com>
2025-10-28 11:40:16 -0300

    DECAF-194 Update after update dependencies

--- src/factory/exceptions/HttpExceptionFilter.ts
+++ src/factory/exceptions/HttpExceptionFilter.ts
@@ -1,21 +1,25 @@
-import {ArgumentsHost, Catch, ExceptionFilter, HttpException} from "@nestjs/common";
-import {Request, Response} from "express";
-import {HttpResponseError} from "./HttpResponseError";
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from "@nestjs/common";
+import { HttpResponseError } from "./HttpResponseError";
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse<Response>();
-        const request = ctx.getRequest<Request>();
-        const status = exception.getStatus();
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
 
-        const httpResponseError = new HttpResponseError(request, {
-            status,
-            message: exception.message,
-            error: exception.name
-        });
+    const httpResponseError = new HttpResponseError(request, {
+      status,
+      message: exception.message,
+      error: exception.name,
+    });
 
-        response.status(httpResponseError.status).json(httpResponseError);
-    }
+    (response as any).status(httpResponseError.status).json(httpResponseError);
+  }
 }
--- src/factory/exceptions/NotFoundExceptionFilter.ts
+++ src/factory/exceptions/NotFoundExceptionFilter.ts
@@ -1,34 +1,38 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from "@nestjs/common";
-import { Request, Response } from "express";
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from "@nestjs/common";
 import { HttpResponseError } from "./HttpResponseError";
 
 export class NotFoundError extends Error {
-	readonly status: number;
-	readonly code: string;
+  readonly status: number;
+  readonly code: string;
 
-	constructor(message = "Resource not found") {
-		super(message);
-		this.name = "NotFoundError";
-		this.status = 404;
-		this.code = "NOT_FOUND";
+  constructor(message = "Resource not found") {
+    super(message);
+    this.name = "NotFoundError";
+    this.status = 404;
+    this.code = "NOT_FOUND";
 
-		Object.setPrototypeOf(this, NotFoundError.prototype);
-	}
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
 }
 
 @Catch(NotFoundError)
 export class NotFoundExceptionFilter implements ExceptionFilter {
-	catch(exception: NotFoundError, host: ArgumentsHost) {
-		const ctx = host.switchToHttp();
-		const response = ctx.getResponse<Response>();
-		const request = ctx.getRequest<Request>();
+  catch(exception: NotFoundError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
 
-		const httpResponseError = new HttpResponseError(request, {
-			status: HttpStatus.NOT_FOUND,
-			message: exception.message,
-			error: "NOT_FOUND",
-		});
+    const httpResponseError = new HttpResponseError(request, {
+      status: HttpStatus.NOT_FOUND,
+      message: exception.message,
+      error: "NOT_FOUND",
+    });
 
-		response.status(httpResponseError.status).json(httpResponseError);
-	}
+    (response as any).status(httpResponseError.status).json(httpResponseError);
+  }
 }
