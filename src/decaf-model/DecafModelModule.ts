02b230ed234f2cbd9005c111136b5b98fa024957
Pedro Costa <pcesar_costa@hotmail.com>
2025-11-26 12:00:06 -0300

    DECAF-202 Add handler feature (#8)
    * DECAF-202 Add decaf handler feature
    
    * DECAF-202 Update docs
    
    * DECAF-202 Minor fix

--- src/decaf-model/DecafModelModule.ts
+++ src/decaf-model/DecafModelModule.ts
@@ -1,14 +1,21 @@
-import { DynamicModule, Module } from "@nestjs/common";
+import { DynamicModule, Module, Type } from "@nestjs/common";
 import { Adapter } from "@decaf-ts/core";
 import { Logging } from "@decaf-ts/logging";
-import { RepoFactory } from "../RepoFactory";
 import { FromModelController } from "./FromModelController";
+import { DecafRequestHandlerInterceptor } from "../interceptors";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { type DecafRequestHandler } from "../types";
+import { DecafHandlerExecutor, DecafRequestContext } from "../request";
+import { DECAF_HANDLERS } from "../constants";
 
 @Module({})
 export class DecafModelModule {
   private static readonly log = Logging.for(DecafModelModule.name);
 
-  static forRoot(flavour: string): DynamicModule {
+  static forRoot(
+    flavour: string,
+    handlers: Type<DecafRequestHandler>[] = []
+  ): DynamicModule {
     const log = this.log.for(this.forRoot);
     log.info(`Generating controllers for flavour...`);
 
@@ -20,7 +27,23 @@ export class DecafModelModule {
     return {
       module: DecafModelModule,
       controllers,
-      providers: [RepoFactory],
+      providers: [
+        {
+          provide: DECAF_HANDLERS,
+          useFactory: () => {
+            return handlers.map((H) => {
+              log.info(`Registered request handler: ${H.name}`);
+              return new H();
+            });
+          },
+        },
+        DecafRequestContext,
+        DecafHandlerExecutor,
+        {
+          provide: APP_INTERCEPTOR,
+          useClass: DecafRequestHandlerInterceptor,
+        },
+      ],
     };
   }
 }
---
+++ src/request/index.ts
@@ -0,0 +1,2 @@
+export * from "./DecafRequestContext";
+export * from "./DecafHandlerExecutor";
