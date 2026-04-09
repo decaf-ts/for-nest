68df7bf1cd4665beb1db460da4d50208b33aae6a
tvenceslau <venceslau.tiago@gmail.com>
2026-04-09 16:41:46 +0100

    patch - DECAF-213 better handling of events-no-ci - after release preparation

--- src/events-module/utils.ts
+++ src/events-module/utils.ts
@@ -1,11 +1,25 @@
+import { Model } from "@decaf-ts/decorator-validation";
+
 export function normalizeEventResponse(args: any[]): unknown[] {
   const [modelConstr, operation, id, payload] = args;
 
   const modelName = modelConstr?.name ?? modelConstr;
 
   const serializedPayload = Array.isArray(payload)
-    ? payload.map((e) => e.serialize())
-    : payload?.serialize();
+    ? payload.map((e) => {
+        try {
+          if (e instanceof Model) return e.serialize();
+          return typeof e === "string" ? e : JSON.stringify(e);
+        } catch (err: unknown) {
+          console.warn(`Failed to serialize payload for ${modelName}: ${err}`);
+          return undefined;
+        }
+      })
+    : payload && payload instanceof Model
+      ? payload.serialize()
+      : payload
+        ? JSON.stringify(payload)
+        : undefined;
 
   return [modelName, operation, id, serializedPayload];
 }
